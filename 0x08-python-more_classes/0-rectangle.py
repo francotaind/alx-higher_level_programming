0b8a91427828187cc1ce2169d216e18ce278730e
francotaind <francotaind00@gmail.com>
2024-01-04 22:25:08 +0300

    test driven development submissions

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,2 +1,8 @@
+#!/usr/bin/pthon3
+#0-recangle.py
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
+    """Represent a rectangle"""
     pass
---
+++ gp
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -m 'classes and objects'
+git push
