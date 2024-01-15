466807bc48f3969eb6f32e8fa2739656eab860dd
francotaind <francotaind00@gmail.com>
2024-01-15 18:32:57 +0300

    tasksubmission#003

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,54 +1,80 @@
-#!/bin/usr/python3
-
+'''
+   Defines the Class Rectangle
+'''
 from models.base import Base
 
+
 class Rectangle(Base):
-    """rectangle class inherits from base."""
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """This is a class constructor for the rectangle"""
-
-        super().__init__(id)
-        #private instances with getters and setters
-
-        self.__width = 0
-        self.__height = 0
-        self.__x = 0
-        self.__Y = 0
-
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value if value > 0 else 0
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value if value > 0 else 0
-        
-        @property
-        def x(self):
-            return self.__x
-        @x.setter
-        def x(self, value):
-            self.__x = value if value >= 0 else 0
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
 
-        @property
-        def y(self):
-            return self.__y
-        @y.setter
-        def y(self, value):
-            self.__y = value if value >= 0 else 0
+    @property
+    def x(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__x
 
+    @x.setter
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
 
