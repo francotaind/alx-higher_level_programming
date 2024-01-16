2d3ded986fc360ae8d76b59e2364c9c2a1902faf
francotaind <francotaind00@gmail.com>
2024-01-16 16:44:30 +0300

    tasksubmission#003

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 """
 Test for the Rectangle class
+Unittest classes:
+    TestRectangle_instantiation - line 25
+    TestRectangle_width - line 114
+    TestRectangle_height - line 190
+    TestRectangle_x - line 262
+    TestRectangle_y - line 334
+    TestRectangle_order_of_initialization - line 402
+    TestRectangle_area - line 430
+    TestRectangle_update_args - line 538
+    TestRectangle_update_kwargs - line 676
+    TestRectangle_to_dictionary - line 788
 """
-
+import sys
 import unittest
 import pep8
 import inspect
-import io
-import json
 import os
+import json
+import io
 from contextlib import redirect_stdout
 from models import rectangle
 from models.base import Base
