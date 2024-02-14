308ac75edafd13dea80bbc9e61118267b56fa9dd
francotaind <francotaind00@gmail.com>
2024-02-14 05:47:17 +0300

    mysql task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 --insert a new row into first_table in a database
 
-INSERT INTO hbtn_0c_0.first_table (id, name) VALUES(89, 'Best School');
+INSERT INTO 'first_table' ('id', 'name') VALUES(89, "Best School");
 
