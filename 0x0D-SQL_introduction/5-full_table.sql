4cce4e5a747b3a4918d9163949a8d2cdda367094
francotaind <francotaind00@gmail.com>
2024-02-14 05:58:26 +0300

    mysql task

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
--- Retrieve the structure of a table from a msql server
+-- script that prints the full description of the table first_table from the database
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first table';
-
+SHOW CREATE TABLE first_table;
