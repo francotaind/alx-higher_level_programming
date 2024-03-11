7f1ae900174313cd928d1a0527697e89d1b5c9f9
francotaind <francotaind00@gmail.com>
2024-03-11 20:58:53 +0300

    javascript code add

---
+++ 0x12-javascript-warm_up/2-arguements.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Check if any arguments are passed
+if (process.argv[2] !== undefined) {
+    console.log(process.argv[2]); // Print the first argument
+} else {
+    console.log("No argument"); // Print if no argument is passed
+}
