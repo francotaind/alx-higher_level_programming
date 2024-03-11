3dbb71678c91f2a82092f9cf45c995fb26a2045c
francotaind <francotaind00@gmail.com>
2024-03-11 22:20:17 +0300

    javascript code add

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Check if the first and second arguments are passed
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-    console.log(process.argv[2] + " is " + process.argv[3]); // Print both arguments in the specified format
+  console.log(process.argv[2] + ' is ' + process.argv[3]); // Print both arguments in the specified format
 } else if (process.argv[2] !== undefined && process.argv[3] === undefined) {
-    console.log(process.argv[2] + " is undefined"); // Print the first argument followed by "is undefined" if only one argument is passed
+  console.log(process.argv[2] + ' is undefined'); // Print the first argument followed by "is undefined" if only one argument is passed
 } else {
-    console.log("undefined is undefined"); // Print "undefined is undefined" if no arguments are passed
+  console.log('undefined is undefined'); // Print "undefined is undefined" if no arguments are passed
 }
