80bba7cfdc809210ce6c8a4e64eb1fbc97050ff9
francotaind <francotaind00@gmail.com>
2024-03-12 11:53:33 +0300

    javascript code add

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const number = parseInt(process.argv[2]);
+const result = factorial(number);
+console.log(result);
