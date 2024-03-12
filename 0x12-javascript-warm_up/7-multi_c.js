c874435b5b7e0ed1b9b2646c575b7bf110629a59
francotaind <francotaind00@gmail.com>
2024-03-12 09:52:16 +0300

    javascript code add

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 
-if (isNaN(number)){
-	console.log('Missing number of occurrences');
-}
-else
-{
-for (let i = 0; i < number; i++){
-	console.log('C is fun');
-}
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
 }
