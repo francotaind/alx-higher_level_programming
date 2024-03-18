ab797654968a5103f71ebc69129856e1346f3cf1
francotaind <francotaind00@gmail.com>
2024-03-18 10:38:33 +0300

    sql database connections

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,41 +1,27 @@
-#!/usr/bin/env python3
-"""Script to list all cities from the database hbtn_0e_4_usa"""
+#!/usr/bin/python3
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
 
-    try:
-        # Connect to MySQL server
-        conn = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+        )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
+    table = cur.fetchall()
 
-        # Create a cursor object
-        cursor = conn.cursor()
+    for row in table:
+        print(row)
 
-        # Execute the query
-        query = "SELECT * FROM cities ORDER BY id ASC"
-        cursor.execute(query)
-
-        # Fetch all rows and print them
-        for row in cursor.fetchall():
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close database connection
-        if 'conn' in locals() and conn.open:
-            conn.close()
-
+    cur.close()
+    db.close()
