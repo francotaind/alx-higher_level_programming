f9c5787737ab43bb0ad0ba32bcbc2bf134546c00
francotaind <francotaind00@gmail.com>
2024-03-18 10:12:16 +0300

    sql database connections

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 """
-Script to list all states with a name starting with N 
-from database `hbtn_0e_0_usa`
+Script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
 import sys
+
 
 if __name__ == '__main__':
 
@@ -17,6 +17,7 @@ if __name__ == '__main__':
         db=sys.argv[3],
         port=3306
         )
+    state_name = sys.argv[4]
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%{}%'\
@@ -28,4 +29,3 @@ if __name__ == '__main__':
 
     cur.close()
     db.close()
-
