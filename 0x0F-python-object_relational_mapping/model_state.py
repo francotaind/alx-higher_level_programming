f847df2c5b86d20f3b52ce1caa4787b1f48ce676
francotaind <francotaind00@gmail.com>
2024-03-18 22:57:38 +0300

    sql database connections

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,12 @@
 
 """Module defining state class and base instance"""
 
+import sqlalchemy
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()
 
 class State(Base):
     """State class
