c6f76397ddf83c523b1130cf8c15a250f5c86a84
francotaind <francotaind00@gmail.com>
2024-03-19 08:11:47 +0300

    sql database connections

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Module defining state class and base instance"""
+
+import sqlalchemy
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = sqlalchemy.orm.declarative_base()
+
+class City(Base):
+    """City class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
