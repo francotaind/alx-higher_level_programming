d0f041ca2a7c8f8d554656b1ec221c327c2562cb
francotaind <francotaind00@gmail.com>
2024-03-28 09:57:15 +0300

    devops_configuration

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ -z "$1" ]; then
-  echo "Error: Please provide a URL as an argument."
-  exit 1
-fi
-
-# Extract the body of the response using curl with silent mode (-s) 
-# and store it in a variable
-response_body=$(curl -s "$1")
-
-# Check for successful curl execution (exit code 0)
-if [ $? -ne 0 ]; then
-  echo "Error: curl failed to execute."
-  exit 1
-fi
-
-# Get the size of the body (in bytes) using string length
-body_size=${#response_body}
-
-# Display the body size
-echo "Body size: $body_size bytes"
-
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
