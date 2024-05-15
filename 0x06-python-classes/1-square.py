91058ede9f6b745113081292add65867cdddcce1
francotaind <francotaind00@gmail.com>
2024-05-15 11:49:24 +0300

    devops_configuration

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-"""A class of a square initializing a private instance"""
+"""
+A class of a square initializing a private instance
+"""
 
 class Square:
+    """
+    __size (int): THe size of a square which is a private attribute
+    """
     def __init__(self, size):
+    """
+    The constructor for the square class.
+    Args:
+        size(int): The size of the square
+    """
         self.__size = size
